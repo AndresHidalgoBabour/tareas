d2cd666a93a69ea705ca717f9e122404a21c3290
AndresHidalgoBabour <108904099+AndresHidalgoBabour@users.noreply.github.com>
2022-08-15 11:25:08 -0300

    Update tarea1.js
    Se arregla funcionalidad del botón siguiente paso-

--- clase-6/tarea1.js
+++ clase-6/tarea1.js
@@ -1,4 +1,4 @@
-document.querySelector('#siguiente').onclick = function(event) {
+document.querySelector('#siguiente-paso').onclick = function(event) {
   const $cantidadIntegrantes = document.querySelector('#cantidad-integrantes');
   const cantidadIntegrantes = Number($cantidadIntegrantes.value);
 
